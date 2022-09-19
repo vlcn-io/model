ed0c4c0c1e845c1270a8e4e12dbee49a7eec5033
Matt <1009003+tantaman@users.noreply.github.com>
2022-09-19 14:15:48 -0400

    test regular promise (no async/await) usage

--- ts/packages/model/src/context/__tests__/asyncContext.test.ts
+++ ts/packages/model/src/context/__tests__/asyncContext.test.ts
@@ -20,3 +20,32 @@ test("async/await", async () => {
 
   await Promise.all([task(), task(), task(), task(), task()]);
 });
+
+test("promise", () => {
+  let c = 0;
+  const task = () => {
+    let prop = ++c;
+    const ctx = {
+      prop,
+    };
+    return newScope(() => {
+      // @ts-ignore
+      expect(prop).toBe(PSD.prop);
+
+      new Promise((resolve) => setTimeout(resolve, Math.random() * 50)).then(
+        () => {
+          // @ts-ignore
+          expect(prop).toBe(PSD.prop);
+        }
+      );
+    }, ctx);
+  };
+
+  return Promise.all([task(), task(), task(), task(), task()]);
+});
+
+// dexie doesn't patch setTimeout or other APIs but that's fine -- users shouldn't be spreading a tx out over time via anything other
+// than promises and async/await.
+// If we do run into a valid use case... we'll figure out how we should patch the required APIs then.
+// Note: could we fix OpenTelemetry based on the Dexie approach?
+//   - https://github.com/aphrodite-sh/acid-memory/commit/fef0d84765dadd26920fc91bb1ac3ed443c59840
